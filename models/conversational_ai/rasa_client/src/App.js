6d1ff8fc6b01c9be07a0b0ad89dba0e81ebc2e47
Mona Henry <rhenry@ucsd.edu>
2023-02-15 06:46:39 -0800

    adding rasa notebooks

---
+++ models/conversational_ai/rasa_client/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ChatBox from './components/ChatBox'
+
+function App() {
+  return (
+    <div>
+    <ChatBox/>
+    </div>
+  );
+}
+
+export default App;
---
+++ models/conversational_ai/rasa_client/src/components/ChatBox.js
@@ -0,0 +1,14 @@
+export default function ChatBox(){
+	return <div>
+	<div>Rasa Client</div>
+	<div></div>
+	<div>
+		<form>
+		<input type="text"/>
+		<button>send</button>
+		</form>
+	</div>
+	</div>
+
+
+}
