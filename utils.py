0c89bcbc32af2040d7633c3a436f3cf5c904c86a
Amol Kale <amol.kale51@gmail.com>
2023-04-03 13:41:57 -0700

    configuration creation

--- utils.py
+++ utils.py
@@ -4,7 +4,7 @@ import os
 def get_config(section_name,config_name):
     # Reading the configuration
     parse = configparser.ConfigParser()
-    config = parse.read(os.path.dirname(__file__)+"/config.ini")
+    parse.read(os.path.dirname(__file__)+"/config.ini")
     config_details = parse.get(section_name, config_name)
     print(str(parse))
     return config_details
