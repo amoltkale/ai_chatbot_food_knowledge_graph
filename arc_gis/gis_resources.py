239ff1e522d375769b29b02e28f7e2122656c39b
Amol Kale <amol.kale51@gmail.com>
2023-04-07 06:01:13 -0700

    Failed attempt at creating San Diego City Business Directory Feature Layer

--- arc_gis/gis_resources.py
+++ arc_gis/gis_resources.py
@@ -1,8 +1,14 @@
 import os
 import pickle
+from arcgis.geocoding import geocode
+
 
 def san_diego_county_zips():
     
     with open(os.path.dirname(__file__)+"/resources/san_diego_county_zips", "rb") as fp:   # Unpickling
         read_list = pickle.load(fp)
     return [int(i) for i in read_list]
+
+def get_lat_long(address):
+    loc = geocode(address)[0]['location']
+    return loc['x'],loc['y']
