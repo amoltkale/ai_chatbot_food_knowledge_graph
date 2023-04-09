9ac7136f065c6e004e742f9b42a8fb77910b31d6
Amol Kale <amol.kale51@gmail.com>
2023-04-09 15:32:47 -0700

    Created all the variable feature layers

--- utils.py
+++ utils.py
@@ -9,3 +9,13 @@ def get_config(section_name,config_name):
     print(str(parse))
     return config_details
     
+def format_json(rings_json):    
+    new_string = ""
+
+    for char in rings_json:
+        if char == "'":
+            new_string += "\""
+        else:
+            new_string += char
+            
+    return new_string
