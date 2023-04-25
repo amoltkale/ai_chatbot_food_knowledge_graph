bf1c569eee915403f7ae4a4326d5fa2372b3f1eb
Matthew S <mpstickle@ucdavis.edu>
2023-04-24 21:32:46 -0700

    Added csv read of exact files to gis_resource. Call functions in notebook to load csv and call queries

--- arc_gis/gis_resources.py
+++ arc_gis/gis_resources.py
@@ -1,13 +1,36 @@
-import os
+from pathlib import Path
+
+import csv
 import pickle
+
 from arcgis.geocoding import geocode
 
-
 def san_diego_county_zips():
-    
-    with open(os.path.dirname(__file__)+"/resources/san_diego_county_zips", "rb") as fp:   # Unpickling
+    '''
+    TODO
+    '''
+    with open(Path(__file__).parent.resolve() / "resources/san_diego_county_zips", "rb") as fp:   # Unpickling
         read_list = pickle.load(fp)
     return read_list
+
+def read_exact_food_biz_categories():
+    with open(Path(__file__).parent.resolve() / "resources/exact_food_categories.csv", "r") as fp:
+        reader = csv.reader(fp)
+        next(reader, None)  # skip the headers
+        res = []
+        for row in reader:
+            res.append(row[0])
+    return res
+
+def read_exact_unhealthy_food_biz_categories():
+    with open(Path(__file__).parent.resolve() / "resources/exact_unhealthy_categories.csv", "r") as fp:   # Unpickling
+        reader = csv.reader(fp)
+        next(reader, None)  # skip the headers
+        res = []
+        for row in reader:
+            res.append(row[0])
+    return res
+
 
 def get_lat_long(address):
     try:
