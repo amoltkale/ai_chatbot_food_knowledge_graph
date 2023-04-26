980b571c504f65cb4b1e451d25987c05aefc0e3d
Amol Kale <amol.kale51@gmail.com>
2023-04-25 19:24:58 -0700

    Created the unhealthy food percentage feature layer.

--- arc_gis/gis_resources.py
+++ arc_gis/gis_resources.py
@@ -3,6 +3,8 @@ from pathlib import Path
 import csv
 import pickle
 
+import psycopg2
+
 from arcgis.geocoding import geocode
 
 def san_diego_county_zips():
@@ -31,6 +33,24 @@ def read_exact_unhealthy_food_biz_categories():
             res.append(row[0])
     return res
 
+def create_where_clause(in_list):
+    qry_where_list = []
+    for c in in_list:
+
+        if "'" in c:
+            where_part = c.replace("'", "''")
+            where_part = "'" + where_part + "' = any(categories) OR "
+        else:
+            where_part = "'" + c + "' = any(categories) OR "
+        qry_where_list.append(where_part)
+    # clean up last or
+    qry_where_list[-1] = qry_where_list[-1].replace(" OR ", "")
+
+    qry_where = "WHERE ("
+    for w in qry_where_list:
+        qry_where = fr"{qry_where}{w} "
+    qry_where = qry_where  + "))"
+    return qry_where
 
 def get_lat_long(address):
     try:
@@ -40,3 +60,22 @@ def get_lat_long(address):
         print(f"Problem with address: {address}")
         ret_val = 0.0,0.0
     return ret_val
+
+def execute_sql(conn, qry):
+    '''
+    This executed the SQL query and returns the output in form of a list.
+    '''
+    cur = conn.cursor()
+
+    # execute a statement
+    cur.execute(qry)
+
+
+    # display the PostgreSQL database server version
+    res = cur.fetchall()
+    # res = [c[0] for c in col_names]
+
+    # Close the communication with the PostgreSQL
+    cur.close()
+    
+    return res
