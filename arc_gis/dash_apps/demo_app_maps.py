df96f439240883d388a2dda5fc327735feff4cb7
Amol Kale <amol.kale51@gmail.com>
2023-04-29 21:43:43 -0700

    dash for map

--- arc_gis/dash_apps/demo_app_maps.py
+++ arc_gis/dash_apps/demo_app_maps.py
@@ -11,7 +11,9 @@ from dash import dcc, html
 
 from langchain.llms import OpenAI
 
-from arcgis_functions import get_block_group_map
+from tools_agent import agent
+
+import json
 
 import os
 
@@ -89,25 +91,25 @@ def update_conversation(click, text):
         return "", conv_hist
     if click > 0:
         # call bot with user inputted text
-        print_statements_file_name = get_block_group_map(text, 2.0)
 
         #response = "Bye Bye"
 
+        output_json = json.loads(agent.run(text))
 
         # user message aligned left
         rcvd = [html.H5(text, style={'text-align': 'left'})]
         
-        rspd =  [html.Iframe(src=print_statements_file_name[1], height="300px", width="500px")]
+        rspd =  [html.Iframe(src=output_json['file_path'], height="300px", width="500px")]
         # response = get_bot_response(llm, text)
 
 
         # bot response aligned right and italics
         # All return functions from langchain should be in a list so that it can be appended
         # rspd = [html.H5(html.I(response), style={'text-align': 'right'})]
-        additional_text = [html.H5(html.I(print_statements_file_name[0]), style={'text-align': 'right'})]
+        additional_text = [html.H5(html.I(output_json['verbal_desc']), style={'text-align': 'right'})]
 
         # append interaction to conversation history
-        conv_hist = conv_hist + additional_text + rcvd + rspd + [html.Hr()]
+        conv_hist = conv_hist + rcvd + rspd + additional_text + [html.Hr()]
         # conv_hist = conv_hist + rcvd + [html.Embed(src='./static/test_html.html')] + [html.Hr()]
 
         return "", conv_hist
