1b4fb3ee24486d942f4e13fc348d677b97efef9e
Matthew S <mpstickle@ucdavis.edu>
2023-06-01 20:29:06 -0700

    Standardized gpt response to be all markdown

--- arc_gis/chatbot_apps/nourish_chatbot_app.py
+++ arc_gis/chatbot_apps/nourish_chatbot_app.py
@@ -82,45 +82,37 @@ else:
 # Flask app 
 server = Flask(__name__)
 app = dash.Dash(name = __name__, server = server)
-# app.css.append_css({"external_url": "https://codepen.io/chriddyp/pen/bWLwgP.css"})
 
 # init a list of the sessions conversation history
-#conv_hist = [html.H5(html.I(intial_response), style={'text-align': 'left'})] + [html.Hr()]
-conv_hist = [html.Hr()] + [html.H5(html.I(intial_response), style=response_style)] + [html.Hr()]
+conv_hist = [html.Hr()] + [dcc.Markdown(intial_response, style=response_style)] + [html.Hr()]
 
 # credit to initial UI: https://github.com/AdamSpannbauer/app_rasa_chat_bot/blob/master/dash_demo_app.py
 
 # app ui
 app.layout = html.Div([
+    html.Div([
     html.Img(src='static/nourish_logo.png', height="100px", width="100px"),
-        html.H3('Nourish Chatbot',style={'text-align': 'center'}),
-        html.H4('Please answer all questions as accurately as possible. If you are unsure, please ask the bot to try again.',
-                style={'text-align':'center'}),
-        html.Div([
-            html.Div(id='conversation'),
-            # html.Br(),
-            # dcc.Loading(
-            #             id="loading-1",
-            #             type="default",
-            #             children=html.Div([
-            #             dcc.Input(id='msg_input', value='', type='text',style={'width':'925px'}),
-            #             html.Button('>', id='send_button', type='submit',style={'width':'40px'})
-            #             ],)
-            #             ),
-            ],
-            id='screen',
-            style={'width': '1000px', 'margin': '0 auto'}),
-        html.Div([
-            html.Br(),
-            dcc.Loading(
-                        id="loading-1",
-                        type="default",
-                        children=html.Div([
-                        dcc.Input(id='msg_input', value='', type='text',style={'width':'925px'}),
-                        html.Button('>', id='send_button', type='submit',style={'width':'40px'})
-                        ],)
-                        ),
-        ], style={'width': '1000px', 'margin': '0 auto'}),
+    html.H3('Nourish Chatbot',style={'text-align': 'center'}),
+    html.H4('Please answer all questions as accurately as possible. If you are unsure, please ask the bot to try again.',
+            style={'text-align':'center'})
+    ]),
+    html.Div([
+        html.Div(id='conversation'),
+        ],
+        id='screen',
+        style={'width': '1000px', 'margin': '0 auto'}
+    ),
+    html.Div([
+        html.Br(),
+        dcc.Loading(
+                    id="loading-1",
+                    type="default",
+                    children=html.Div([
+                    dcc.Input(id='msg_input', value='', type='text',style={'width':'925px'}),
+                    html.Button('>>>', id='send_button', type='submit',style={'width':'60px'})
+                    ],)
+                    ),
+    ], style={'width': '1000px', 'margin': '0 auto'}),
 ])
 
 # trigger bot response to user inputted message on submit button click
@@ -149,10 +141,10 @@ def update_conversation(click, text):
             try:
                 output_json = json.loads(agent_response)
                 if 'file_path' in output_json:
-                    additional_text = [html.H5(html.I(output_json['verbal_desc']), style=response_style)]
+                    additional_text = [dcc.Markdown(output_json['verbal_desc'], style=response_style)]
                     rspd =  [html.Iframe(src=output_json['file_path'], height="400px", width="1000px")] + additional_text
                 else:
-                    rspd = [html.H5(html.I(output_json['response']), style=response_style)]
+                    rspd = [dcc.Markdown(output_json['response'], style=response_style)]
             except:
                 rspd = [dcc.Markdown(agent_response, style=response_style)]
         else:
